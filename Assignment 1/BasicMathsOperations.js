78fc6608f664b02cc9ba953e6e67c2247cc7b26b
Sushil Landge <sushil_landge@epam.com>
2017-10-23 12:06:18 +0530

    ArrayToString.js after Assignment 1 review

--- Assignment 1/BasicMathsOperations.js
+++ Assignment 1/BasicMathsOperations.js
@@ -1,14 +1,9 @@
-function basicOp(operation, value1, value2)
-{
+function basicOp(operation, value1, value2) {
   switch(operation) {
   case '+' : return value1 + value2;
-  break;
   case '-' : return value1 - value2;
-  break;
   case '*' : return value1 * value2;
-  break;
   case '/' : return value1 / value2;
-  break;
   default : 
   return "Entered Combination is Wrong, Please enter an operator, operand1, operand2";
   }

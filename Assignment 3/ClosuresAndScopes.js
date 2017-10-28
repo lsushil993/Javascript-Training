79f25f61820e2d65a13245e9c7cae1ba81093dcc
Sushil Landge <sushil_landge@epam.com>
2017-10-28 13:04:05 +0530

    Native Javascript Assignment 3

---
+++ Assignment 3/ClosuresAndScopes.js
@@ -0,0 +1,12 @@
+function createFunctions(n) {
+  var callbacks = [];
+  
+  for (var i=0; i<n; i++) {
+    var func = (function(x) {
+        return function() { return x;}
+    })(i);
+    
+    callbacks.push(func);
+  }
+    return callbacks;
+}

c3e79014a26d5fb80327755d1f1bf42107b22717
Sushil Landge <sushil_landge@epam.com>
2017-11-12 11:25:46 +0530

    Assignment 1 after Review/Arithmetic Operations.js

--- Assignment 1 after Review/Arithmetic Operations.js
+++ Assignment 1 after Review/Arithmetic Operations.js
@@ -1,3 +1,4 @@
-function basicOp(operation, value1, value2) {
-  return eval(value1 + operation + value2);
+function basicOp(operation, value1, value2)
+{
+  return (operation == "+" ? value1 + value2 : operation == "-" ? value1 - value2 : operation == "*" ? value1 * value2 : operation == "/" ? value1 / value2 : "Invalid Operation");
 }
